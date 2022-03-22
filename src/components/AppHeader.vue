964272ba6b788018e5886d49407c66e505487774
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-22 16:22:35 +0100

    creata la nav bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,77 @@
 <template>
-  <header></header>
+  <header>
+    <nav>
+      <ul>
+        <li v-for="link in links" :key="link.text">
+          <a href="link.href">{{ link.text }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
   name: "AppHeader",
+  data() {
+    return {
+      links: [
+        {
+          href: "#",
+          text: "Characters",
+        },
+        {
+          href: "#",
+          text: "Comics",
+        },
+        {
+          href: "#",
+          text: "Movies",
+        },
+        {
+          href: "#",
+          text: "TV",
+        },
+        {
+          href: "#",
+          text: "Games",
+        },
+        {
+          href: "#",
+          text: "Collectibles",
+        },
+        {
+          href: "#",
+          text: "Videos",
+        },
+        {
+          href: "#",
+          text: "Fans",
+        },
+        {
+          href: "#",
+          text: "News",
+        },
+        {
+          href: "#",
+          text: "Shop",
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+ul {
+  list-style: none;
+  li {
+    display: inline-block;
+    margin: 1em;
+    a {
+      display: inline-block;
+      text-decoration: none;
+    }
+  }
+}
+</style>
